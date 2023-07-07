6b359d3ca2600acad2d5ba00d87417f843dfb958
conbanwa <j6j7@qq.com>
2023-07-08 01:03:41 +0800

    Update sys_init.go

--- server/model/system/request/sys_init.go
+++ server/model/system/request/sys_init.go
@@ -7,13 +7,14 @@ import (
 )
 
 type InitDB struct {
-	DBType   string `json:"dbType"`                    // 数据库类型
-	Host     string `json:"host"`                      // 服务器地址
-	Port     string `json:"port"`                      // 数据库连接端口
-	UserName string `json:"userName"`                  // 数据库用户名
-	Password string `json:"password"`                  // 数据库密码
-	DBName   string `json:"dbName" binding:"required"` // 数据库名
-	DBPath   string `json:"dbPath"`                    // sqlite数据库文件路径
+	DBType   string `json:"dbType"`                      // 数据库类型
+	Host     string `json:"host"`                        // 服务器地址
+	Port     string `json:"port"`                        // 数据库连接端口
+	UserName string `json:"userName" binding:"required"` // 数据库用户名
+	Password string `json:"password"`                    // 数据库密码
+	DBName   string `json:"dbName" binding:"required"`   // 数据库名
+	DBPath   string `json:"dbPath"`                      // sqlite数据库文件路径
+	UseTls   bool   `json:"useTls"`                      // 是否开启TLS
 }
 
 // MysqlEmptyDsn msyql 空数据库 建库链接
@@ -25,7 +26,11 @@ func (i *InitDB) MysqlEmptyDsn() string {
 	if i.Port == "" {
 		i.Port = "3306"
 	}
-	return fmt.Sprintf("%s:%s@tcp(%s:%s)/", i.UserName, i.Password, i.Host, i.Port)
+	initTls := ""
+	if i.UseTls {
+		initTls = "?tls=true"
+	}
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", i.UserName, i.Password, i.Host, i.Port, initTls)
 }
 
 // PgsqlEmptyDsn pgsql 空数据库 建库链接
@@ -37,7 +42,11 @@ func (i *InitDB) PgsqlEmptyDsn() string {
 	if i.Port == "" {
 		i.Port = "5432"
 	}
-	return "host=" + i.Host + " user=" + i.UserName + " password=" + i.Password + " port=" + i.Port + " dbname=" + "postgres" + " " + "sslmode=disable TimeZone=Asia/Shanghai"
+	initTls := "sslmode=disable"
+	if i.UseTls {
+		initTls = "sslmode=enable"
+	}
+	return "host=" + i.Host + " user=" + i.UserName + " password=" + i.Password + " port=" + i.Port + " dbname=" + "postgres" + " " + initTls + " TimeZone=Asia/Shanghai"
 }
 
 // SqliteEmptyDsn sqlite 空数据库 建库链接
@@ -49,6 +58,10 @@ func (i *InitDB) SqliteEmptyDsn() string {
 // ToMysqlConfig 转换 config.Mysql
 // Author [SliverHorn](https://github.com/SliverHorn)
 func (i *InitDB) ToMysqlConfig() config.Mysql {
+	initTls := ""
+	if i.UseTls {
+		initTls = "?tls=true"
+	}
 	return config.Mysql{
 		GeneralDB: config.GeneralDB{
 			Path:         i.Host,
@@ -59,7 +72,7 @@ func (i *InitDB) ToMysqlConfig() config.Mysql {
 			MaxIdleConns: 10,
 			MaxOpenConns: 100,
 			LogMode:      "error",
-			Config:       "charset=utf8mb4&parseTime=True&loc=Local",
+			Config:       "charset=utf8mb4&parseTime=True&loc=Local" + initTls,
 		},
 	}
 }
@@ -67,6 +80,10 @@ func (i *InitDB) ToMysqlConfig() config.Mysql {
 // ToPgsqlConfig 转换 config.Pgsql
 // Author [SliverHorn](https://github.com/SliverHorn)
 func (i *InitDB) ToPgsqlConfig() config.Pgsql {
+	initTls := "sslmode=disable"
+	if i.UseTls {
+		initTls = "sslmode=enable"
+	}
 	return config.Pgsql{
 		GeneralDB: config.GeneralDB{
 			Path:         i.Host,
@@ -77,7 +94,7 @@ func (i *InitDB) ToPgsqlConfig() config.Pgsql {
 			MaxIdleConns: 10,
 			MaxOpenConns: 100,
 			LogMode:      "error",
-			Config:       "sslmode=disable TimeZone=Asia/Shanghai",
+			Config:       initTls + " TimeZone=Asia/Shanghai",
 		},
 	}
 }
